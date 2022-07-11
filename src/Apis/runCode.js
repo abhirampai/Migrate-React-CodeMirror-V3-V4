4a8fc4eb2fa241d96036b2f80c52c52c2d2840a7
Abhiram Pai <abhirampai1999@gmail.com>
2022-07-11 13:26:09 +0530

    Fixes

--- src/Apis/runCode.js
+++ src/Apis/runCode.js
@@ -3,17 +3,11 @@ import axios from "axios";
 const clientId = process.env.REACT_APP_JDOODLE_CLIENT_ID;
 const clientSecret = process.env.REACT_APP_JDOODLE_CLIENT_SECRET;
 
+const base_url = "https://api.jdoodle.com/v1/execute";
+
 export const runCode = (payload) =>
-  axios.post(
-    "https://api.jdoodle.com/v1/execute",
-    {
-      ...payload,
-      clientId,
-      clientSecret
-    },
-    {
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8"
-      }
-    }
-  );
+  axios.post(base_url, {
+    ...payload,
+    clientId,
+    clientSecret,
+  });
